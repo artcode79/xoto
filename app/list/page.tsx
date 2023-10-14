6e54d523cc188fb4a180cbe8f7c3d51b39da34dd
artcode79 <uewfau@gmail.com>
2023-10-14 12:45:42 +0700

    tg

---
+++ app/list/page.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "XotoNime | List",
+  description: "List of anime",
+};
+
+const Page = async () => {
+  return (
+    <>
+      <h1>List</h1>
+    </>
+  );
+};
+
+export default Page;
