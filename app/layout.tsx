6aca8388e46cda3b69333a8e7c31bfe779155e2b
artcode79 <uewfau@gmail.com>
2023-10-14 12:07:21 +0700

    y

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Headers from "@/components/Headers";
 import Footers from "@/components/Footers";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         </header>
         {children}
         <Footers />
+        <Analytics />
       </body>
     </html>
   );
